d21e1707194fd1b0db78a9938c5c5b58ef228c83
Abhishek Raina <143702712+Abhishekraina7@users.noreply.github.com>
2026-04-26 11:17:53 +0530

    Secret kept secret

--- src/backend/firebase/firebase.ts
+++ src/backend/firebase/firebase.ts
@@ -4,23 +4,21 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDoP_iT1RNMrQT0EdLT2f2Unb2PYD09MpY",
-  authDomain: "resume-builder-f0154.firebaseapp.com",
-  projectId: "resume-builder-f0154",
-  storageBucket: "resume-builder-f0154.firebasestorage.app",
-  messagingSenderId: "397810368619",
-  appId: "1:397810368619:web:b723bd8b8c125d8632b12e",
-  measurementId: "G-7DN1N847BL"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase safely in Next.js (ensuring it only happens once)
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
-// Core Services
+
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-// Analytics needs to run only in the browser context
 let analytics = null;
 if (typeof window !== "undefined") {
   isSupported().then((supported) => {
